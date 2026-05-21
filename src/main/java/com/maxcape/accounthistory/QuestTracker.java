3fcb7c470f97a35a4f5a1fe1a1d12caac18bab76
Joseph Rollins <rollins.joseph@gmail.com>
2026-05-20 21:16:34 -0700

    Handle uncaught exception in shutdown

--- src/main/java/com/maxcape/accounthistory/QuestTracker.java
+++ src/main/java/com/maxcape/accounthistory/QuestTracker.java
@@ -64,7 +64,13 @@ class QuestTracker extends BaseTracker {
 	void flush() {
 		if (ticksElapsed >= 0) {
 			ticksElapsed = -1;
-			checkQuestStates();
+			try {
+				checkQuestStates();
+			} catch (Exception e) {
+				// quest.getState will throw during plugin shutdown, silently ignore since we
+				// cannot do anything about it at this point
+			}
+
 		}
 	}
 
