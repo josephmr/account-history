f8bb5246cec60c5033d363cfc3a16d773c68b6e7
Joseph Rollins <rollins.joseph@gmail.com>
2026-05-20 17:48:07 -0700

    Fix account identify tracking

--- src/main/java/com/maxcape/accounthistory/AccountIdentifyTracker.java
+++ src/main/java/com/maxcape/accounthistory/AccountIdentifyTracker.java
@@ -2,7 +2,6 @@ package com.maxcape.accounthistory;
 
 import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.GameState;
 import net.runelite.api.events.GameStateChanged;
 import net.runelite.api.events.GameTick;
 import okhttp3.OkHttpClient;
@@ -11,33 +10,37 @@ import java.io.File;
 import java.util.Map;
 
 @Slf4j(topic = "maxcape.AccountIdentifyTracker")
-class AccountIdentifyTracker extends BaseTracker
-{
+class AccountIdentifyTracker extends BaseTracker {
 	private boolean shouldSendIdentify = true;
+	private boolean pendingLogin = false;
 
 	AccountIdentifyTracker(AccountHistoryPlugin plugin, AccountHistoryConfig config,
-						   OkHttpClient httpClient, Gson gson, File storeFile)
-	{
+			OkHttpClient httpClient, Gson gson, File storeFile) {
 		super(plugin, config, httpClient, gson, storeFile);
 	}
 
 	@Override
-	void onGameStateChanged(GameStateChanged event)
-	{
-		if (event.getGameState() == GameState.LOGGED_IN)
-		{
-			shouldSendIdentify = true;
-		} else if (event.getGameState() == GameState.LOGIN_SCREEN || event.getGameState() == GameState.HOPPING)
-		{
-			shouldSendIdentify = false;
+	void onGameStateChanged(GameStateChanged event) {
+		switch (event.getGameState()) {
+			case LOGIN_SCREEN:
+			case HOPPING:
+				pendingLogin = true;
+				shouldSendIdentify = false;
+				break;
+			case LOGGED_IN:
+				if (pendingLogin) {
+					shouldSendIdentify = true;
+					pendingLogin = false;
+				}
+				break;
+			default:
+				break;
 		}
 	}
 
 	@Override
-	void onGameTick(GameTick tick)
-	{
-		if (shouldSendIdentify && plugin.getPlayerName() != null)
-		{
+	void onGameTick(GameTick tick) {
+		if (shouldSendIdentify && plugin.getPlayerName() != null) {
 			log.debug("Identifying account: {} -- {}", plugin.getPlayerName(), plugin.getAccountHash());
 			sendEvent("ACCOUNT_IDENTIFY", Map.of());
 			shouldSendIdentify = false;
